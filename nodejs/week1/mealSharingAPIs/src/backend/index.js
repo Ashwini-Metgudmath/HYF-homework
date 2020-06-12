4345099be050e414a3dfc6598ffe958a6caba104
unknown <metgudmath.ashwini7@gmail.com>
2020-06-12 16:08:59 +0200

    added all latest changes for week1 HW

--- nodejs/week1/mealSharingAPIs/src/backend/index.js
+++ nodejs/week1/mealSharingAPIs/src/backend/index.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const app = express();
 
+/*app.use((req, res, next) => {
+    console.log("method " + req.method);
+    next();
+});
+app.use((req, res, next) => {
+    console.log("date " + Date.now());
+    next();
+});*/
+
 const mealsRouter = require('../backend/routes/meals');
 app.use('/meals', mealsRouter);
+
+const cheapMealsRouter = require('./routes/cheapMeals');
+app.use('/cheap-meals', cheapMealsRouter);
+
+const largeMealsRouter = require('./routes/largeMeals');
+app.use('/large-meals', largeMealsRouter);
+
+const mealRouter = require('./routes/meal');
+app.use('/meal', mealRouter);
+
+const allReservationsRouter = require('./routes/reservations');
+app.use('/reservations', allReservationsRouter);
+
+const reservationRouter = require('./routes/reservation');
+app.use('/reservation', reservationRouter);
 
 
 
