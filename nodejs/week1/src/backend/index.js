567492523dfbeef53c9d3a2ce54b12a7aca9fe4e
unknown <metgudmath.ashwini7@gmail.com>
2020-06-13 20:53:31 +0200

    modified cheap and large meals logic

--- nodejs/week1/src/backend/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-const mealsRouter = require('../backend/routes/meals');
-app.use('/meals', mealsRouter);
-
-
-
-app.listen(3000, () => console.log('Server started...'));
